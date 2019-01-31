e06999cfc237b1a0e0275a9b8be2c5c91b5427f7
n4a3 <n4a3>
2019-01-31 22:07:02 +0400

    add smooth scrolling

---
+++ source/blocks/first/first.js
@@ -0,0 +1,12 @@
+var buttonDown = document.querySelector('.button_down');
+
+buttonDown.addEventListener('click', function (e) {
+  e.preventDefault();
+  
+  var blockID = buttonDown.getAttribute('href');
+  
+  document.querySelector('' + blockID).scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
+});
